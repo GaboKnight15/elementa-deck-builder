94a933d02f6c99c0c97a3695bd4caafd9ee727cd
Gabo <gabocaballero2014@gmail.com>
2025-06-10 13:41:16 -0500

    Update server.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
