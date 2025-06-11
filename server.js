9f66ee0a7f5066a2bf077a865147eada64e1c291
Gabo <gabocaballero2014@gmail.com>
2025-06-10 19:12:15 -0500

    Update server.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(express.static('public')); // Serve files from the "public" folder
-
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-
-  // Example: relay messages
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+app.get('/', (req, res) => res.send('Hello from Railway!'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
