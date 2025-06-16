7caad2e5c200c9bd2da664e5f7b4802096f072f1
Gabo <gabocaballero2014@gmail.com>
2025-06-16 06:55:45 -0500

    Update server.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
   });
-
+socket.on('game message', (roomId, msg) => {
+  socket.to(roomId).emit('game message', msg);
+});
   // Example: relay messages
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
@@ -24,4 +26,26 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
+});
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('join room', (roomId) => {
+    socket.join(roomId);
+    // Notify others in the room
+    socket.to(roomId).emit('opponent joined', socket.id);
+    console.log(`Socket ${socket.id} joined room ${roomId}`);
+  });
+
+  // Relay game actions to other player in the room
+  socket.on('game action', (roomId, action) => {
+    socket.to(roomId).emit('game action', action);
+  });
+  socket.on('sync deck', (roomId, deckObj) => {
+  socket.to(roomId).emit('sync deck', deckObj);
+});
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
 });
