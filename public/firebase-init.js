83c9e6a34baf88f2be51640602a19d62a9af1a05
Gabo <gabocaballero2014@gmail.com>
2025-06-18 07:24:19 -0500

    Update firebase-init.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyrTkqm_ksTU4dC3ul_cXQc596VptdhXY",
+  authDomain: "elementa-31166.firebaseapp.com",
+  projectId: "elementa-31166",
+  storageBucket: "elementa-31166.firebasestorage.app",
+  messagingSenderId: "486533994899",
+  appId: "1:486533994899:web:88b25a66cbab1731ab7066"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
