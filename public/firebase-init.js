40e4bc793170bdcc519bec2d0bd6731571afcb64
Gabo <gabocaballero2014@gmail.com>
2025-06-18 07:27:59 -0500

    Update firebase-init.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// firebase-init.js
 const firebaseConfig = {
   apiKey: "AIzaSyAyrTkqm_ksTU4dC3ul_cXQc596VptdhXY",
   authDomain: "elementa-31166.firebaseapp.com",
   projectId: "elementa-31166",
-  storageBucket: "elementa-31166.firebasestorage.app",
+  storageBucket: "elementa-31166.appspot.com", // fix typo: should be .appspot.com
   messagingSenderId: "486533994899",
   appId: "1:486533994899:web:88b25a66cbab1731ab7066"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+// Make auth global so auth.js can use it
+window.auth = firebase.auth();
