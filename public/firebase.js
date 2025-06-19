eb9f2bf9543b6c0ced4626747e7f5960c0b256b5
Gabo <gabocaballero2014@gmail.com>
2025-06-18 21:41:33 -0500

    Update firebase.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- public/firebase.js
+++ public/firebase.js
@@ -1,1 +1,10 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAyrTkqm_ksTU4dC3ul_cXQc596VptdhXY",
+  authDomain: "elementa-31166.firebaseapp.com",
+  projectId: "elementa-31166",
+  storageBucket: "elementa-31166.appspot.com",
+  messagingSenderId: "486533994899",
+  appId: "1:486533994899:web:88b25a66cbab1731ab7066"
+};
+firebase.initializeApp(firebaseConfig);
+window.auth = firebase.auth();
