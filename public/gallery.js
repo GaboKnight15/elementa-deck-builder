553741c2f4743027ca72bca206e1764120d25aca
Gabo <gabocaballero2014@gmail.com>
2025-06-22 11:01:13 -0500

    Update gallery.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- public/gallery.js
+++ public/gallery.js
@@ -113,8 +113,7 @@ function renderGallery() {
       document.body.classList.add(`theme-${color}`);
     }
   });
-window.renderGallery = renderGallery;
 // ==========================
 // === INITIALIZATION ===
 // ==========================
-renderGallery();
+window.renderGallery = renderGallery;
