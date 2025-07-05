8f88648498f601cc29d3fbc090a6087e7bf27af0
Gabo <gabocaballero2014@gmail.com>
2025-07-04 21:39:47 -0500

    Update gallery.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- public/gallery.js
+++ public/gallery.js
@@ -85,7 +85,7 @@ function createCardGallery(card) {
       playerCollection = collection;
       playerEssence -= cost;
       updateEssenceDisplay();  
-      guardedSaveProgress();
+      saveProgress();
 
       // Mark as new if previously not owned
       if (!wasOwned && collection[card.id] > 0) {
@@ -117,7 +117,7 @@ function createCardGallery(card) {
       playerCollection = collection;
       playerEssence += refund;
       updateEssenceDisplay();
-      guardedSaveProgress();
+      saveProgress();
       renderGallery();
     };
     actionRow.appendChild(voidBtnImg);
