2e097cea6d2d08a487fd578b9b19ece26ee601e4
Gabo <gabocaballero2014@gmail.com>
2025-10-21 02:28:54 -0500

    Update server.js
    Signed-off-by: Gabo <gabocaballero2014@gmail.com>

--- server.js
+++ server.js
@@ -91,22 +91,14 @@ socket.on('profile', (profileObj) => {
     socket.to(socket.roomId).emit('opponent profile', profileObj);
   }
 });
-// Add to rooms structure: rooms[roomId].champions = { [socket.id]: championData }
-socket.on('champion-selected', (roomId, championData) => {
-  if (!rooms[roomId].champions) rooms[roomId].champions = {};
-  rooms[roomId].champions[socket.id] = championData;
 
-  // If both have chosen, notify both clients
-  if (Object.keys(rooms[roomId].champions).length === 2) {
-    io.to(roomId).emit('both-champions-selected', rooms[roomId].champions);
-  }
-});
 socket.on('game message', (roomId, msg) => {
   io.to(roomId).emit('game message', {
     sender: socket.username,
     msg
   });
 });
+  
 socket.on('game action log', (roomId, logData) => {
   io.to(roomId).emit('game action log', logData);
 });
